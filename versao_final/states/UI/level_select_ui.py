6cfd332fe6dffaadef1db10db499b095cf9f1ca6
Marcos Vinicius <marcos2005.machado@gmail.com>
2023-11-18 16:18:53 -0300

    Created basic level selecting logic

--- versao_final/states/UI/level_select_ui.py
+++ versao_final/states/UI/level_select_ui.py
@@ -1,13 +1,17 @@
+from __future__ import annotations
 from states.UI.ui import UI
-from game import Game
 from buttons.text_button import TextButton
 import pygame
 import states.in_game_state as in_game_state
+import states.menu_state as menu_state
 class LevelSelectUI(UI):
-    def __init__(self, context: Game) -> None:
+    def __init__(self, context) -> None:
         super().__init__(context)
         self.buttons = [ 
-            TextButton(pygame.font.Font('assets/fonts/Pixeltype.ttf',25).render('Jogar',True,'black'),'green',pygame.Rect(200,700,50,20),lambda: context.change_state(in_game_state.InGameState(context)))
+            TextButton(pygame.font.Font('assets/fonts/Pixeltype.ttf',42).render(' Fase1',True,'black'),'green',pygame.Rect(600,400,75,35),lambda: context.change_state(in_game_state.InGameState(context))),
+            TextButton(pygame.font.Font('assets/fonts/Pixeltype.ttf',42).render(' Retornar',True,'black'),'red',pygame.Rect(600,800,125,35),lambda: context.change_state(menu_state.MenuState(context)))
         ]
     def render(self, screen):
         screen.fill('white')
+        for button in self.buttons:
+            button.draw(screen)
--- versao_final/states/level_select_state.py
+++ versao_final/states/level_select_state.py
@@ -1,6 +1,19 @@
 from states.state import State
-from versao_final.states.UI.ui import UI
+from states.UI.level_select_ui import LevelSelectUI
+import pygame
+import sys
 
 class LevelSelectState(State):
-    def __init__(self, context, UI: UI):
-        super().__init__(context, UI)
+    def __init__(self, context):
+        super().__init__(context, LevelSelectUI(context))
+    
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.check_buttons(pygame.mouse.get_pos())
+    def update(self, delta_time: float) -> None:
+        return
