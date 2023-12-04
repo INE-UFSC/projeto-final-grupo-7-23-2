1b51c23a18b28474c7fe39ea3b6b570af88203d8
Marcos Vinicius <marcos2005.machado@gmail.com>
2023-12-04 20:43:06 -0300

    adaptei as torres para vários inimigos

--- versao_final/entities/enemies/wolf.py
+++ versao_final/entities/enemies/wolf.py
@@ -5,4 +5,4 @@ from singletons.constants import Constants as C
 class Wolf(Enemy):
     def __init__(self, path: Path):
         image_paths = C().get_enemy_sprites('wolf', 6)
-        super().__init__(path, 250.0, 120.0, image_paths, 15)
+        super().__init__(path, 50.0, 120.0, image_paths, 15)
--- versao_final/shop.py
+++ versao_final/shop.py
@@ -1,21 +1,22 @@
+from __future__ import annotations
 import pygame
+
 from factories.tower_factory import TowerFactory
 from singletons.constants import Constants as C
-from entities.enemies.enemy import Enemy
-from states.state import State
+import states.in_game_state as in_game_state
 
 class Shop:
-    def __init__(self, state: State, money: float) -> None:
+    def __init__(self, state: in_game_state.InGameState, money: float) -> None:
         self.__state = state
-        self.__tower_factory = TowerFactory(50, 75, 1.5, 50, self.__state.get_projectiles(), 100)
+        self.__tower_factory = TowerFactory(25, 125, 1.5, 50, 100,  self.__state.get_projectiles(), self.__state.get_enemies())
         self.__money = money
     
-    def add_tower(self, target: Enemy, position: tuple[int,int]):
+    def add_tower(self, position: tuple[int,int]):
         c = C()
         tile_x = position[0] // c.get_tile_size() * c.get_tile_size() + c.get_tile_size() / 2
         tile_y = position[1] // c.get_tile_size() * c.get_tile_size() + c.get_tile_size() / 2
         new_tower_position = pygame.Vector2(tile_x, tile_y)
-        return self.__tower_factory.create_tower(target, new_tower_position)
+        return self.__tower_factory.create_tower(new_tower_position)
     
     def can_buy_tower(self) -> bool:
         return self.__money >= self.__tower_factory.get_tower_price()
